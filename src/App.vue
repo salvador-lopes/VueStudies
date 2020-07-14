e04f6e46fb6ea33b8ddfde37e54302095e31ac34
salvador-lopes <junior.salvador@tinnnova.com.br>
2020-07-14 13:39:56 -0300

    parquivos iniciais

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+
+    <ul>
+      <li v-for="foto of fotos">
+        <img :src="foto.url" :alt="foto.titulo">
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+  data() {
+
+    return {
+
+      titulo: 'Alurapic', 
+      fotos: [
+        {
+          url: 'http://tudosobrecachorros.com.br/wp-content/uploads/cachorro-independente.jpg',
+          titulo: 'cachorro'
+        },
+        {
+          url: 'http://tudosobrecachorros.com.br/wp-content/uploads/cachorro-independente.jpg',
+          titulo: 'Cachorrão'
+        }
+      ]
+    }
+  }
+}
+
+</script>
+
+<style>
+</style>
